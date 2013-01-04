bfc1bb31f9175d9944e1b26263aee8e0267b51a4
Robin Berjon <robin@berjon.com>
2013-01-04 22:44:29 +0100

    start rendering ruby

--- playground/render-ruby/render-ruby.js
+++ playground/render-ruby/render-ruby.js
@@ -8,12 +8,48 @@
         // ,   data = Inception.Elements.Ruby.segmentAndCategoriseRubyElement($ruby[0])
         ,   data = Inception.Elements.Ruby.newSegmentAndCategoriseRubyElement($ruby[0])
         ;
-        console.log(data);
+        // console.log(data);
         
         // simple dump
         var $pre = $("<pre></pre>");
         $pre.text(dump(data));
         $pre.insertAfter($cont);
+
+        // show source
+        var $src = $("<pre></pre>");
+        $src.text($ruby.html());
+        $src.insertAfter($cont);
+
+        // home made rendering
+        var $rootTable = $("<table></table>")
+        ,   $rootTR = $("<tr></tr>")
+        ;
+        $rootTable.append($rootTR);
+        for (var i = 0, n = data.length; i < n; i++) {
+            var run = data[i]
+            ,   max = Math.max( run.bases.length
+                            ,   run.firstCompoundAnnotations.annotations.length
+                            ,   run.secondCompoundAnnotations.annotations.length)
+            ,   $td = $("<td></td>")
+            ,   $table = $("<table></table>")
+            ,   $firstAnnTR = $("<tr></tr>")
+            ,   $baseTR = $("<tr></tr>")
+            ,   $secondAnnTR = $("<tr></tr>")
+            ;
+            $table.append($firstAnnTR);
+            $table.append($baseTR);
+            $table.append($secondAnnTR);
+            
+            // for each first annotation, create a td, clone the range into it, append to TR
+            // for each base, create a td, clone the range into it, append to TR
+            // for each second annotation, create a td, clone the range into it, append to TR
+            // in the last td of each annotation extend colspan if needed
+            // be careful that ranges can be null
+            // range.cloneContents()
+
+            $rootTR.append($td);
+        }
+        $rootTable.appendTo($cont.parent());
     }
     function dump (runs) {
         var str = "";
